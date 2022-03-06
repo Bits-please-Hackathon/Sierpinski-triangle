e1d2cc47b84c7eeea96592c8a91f75c978385775
Marko Bozic <markobozic346@gmail.com>
2022-03-06 08:38:33 +0100

    Fix canvas values, clear canvas before new drawing

--- utils.js
+++ utils.js
@@ -4,9 +4,17 @@ function passValues(depth){
             return;
         }
 
-    var ctx=document.getElementById("triangle-box").getContext("2d");
+    const ctx=document.getElementById("triangle-box").getContext("2d");
     
-    drawTriangle(50,365.47,450,365.47,250,19.06,depth,ctx);
+    
+    //clear canvas before new drawing
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    ctx.beginPath();
+
+    //draw triangle
+    drawTriangle(50,366,450,366,250,20,depth,ctx);
+    
+    //line style
     ctx.strokeStyle = 'black';
     ctx.lineWidth = 2;
     ctx.stroke();
@@ -15,7 +23,6 @@ function passValues(depth){
 
 
 function drawTriangle(Ax,Ay,Bx,By,Cx,Cy,depth,ctx) {
-
     if(depth>0) {
         drawTriangle(Ax,Ay,(Ax + Cx) / 2,(Ay + Cy) / 2,(Ax + Bx) / 2,(Ay + By) / 2,depth-1,ctx);
         drawTriangle((Ax + Bx) / 2,(Ay + By) / 2,(Bx + Cx) / 2,(By + Cy) / 2,Bx,By,depth-1,ctx);
